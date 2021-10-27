7ca51565c444c1f20b02f35d7c79244bd27a1e5e
Kopanef <henrykopanev@gmail.com>
2021-10-27 21:41:04 +0300

    First commit

---
+++ js/archive.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// alert('hello')
+
+// const result = confirm('Are you here');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(typeof(answer + 5));
+
+const answers = [];
+
+// answers[0] = prompt ('Как ваше имя', '')
+// answers[1] = prompt ('Фамилия', '')
+// answers[3] = prompt ('Сколько вам лет', '')
+
+console.log(typeof(answers));  
+console.log(typeof(null));  
+
+
+// ----------------
+
+
+// 8 урок. Интерполяция. Вставка значения внутри строки
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category)
+// // Старый стандарт с конкатеннацией
+
+// console.log(`https://someurl.com/`${category}/5)
+// // Новый стандарт с бектиками
+
+// const user = 'Andrew'
+// alert(`Hello, ${user}`)
